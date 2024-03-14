fa553c6b6398b1a30db236e3355fdbd13edfe7e5
fkeyz <fkeyzuwu@gmail.com>
2024-03-15 01:35:51 +0200

    testing

--- command_runner.gd
+++ command_runner.gd
@@ -2,6 +2,8 @@ class_name CommandRunner extends LineEdit
 
 signal entered_command(command: String)
 
+var test := "lol"
+
 func _on_text_submitted(new_text: String) -> void:
 	entered_command.emit(new_text)
 	clear()
